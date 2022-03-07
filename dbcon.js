302bc8aef81503e74101fb430c0fba002b45ee5a
Chris Jacobs <jacobsc2@flip1.engr.oregonstate.edu>
2022-03-07 11:00:36 -0800

    git init with dbcon.js and empty app.js

---
+++ dbcon.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'classmysql.engr.oregonstate.edu',
+  user            : 'cs340_jacobsc2',
+  password        : '8173',
+  database        : 'cs340_jacobsc2'
+});
+module.exports.pool = pool;
